3526f5b676b7860bf856a6330c5b4843c9ce1a1b
Kirito-1 <1487072880.com>
2019-11-22 00:51:27 -0800

    added a ex02.c flie

---
+++ ex02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    printf("用户输入为:\n");
+    char c[10];
+    int x,y;
+while ((c[x]=getchar())== '\n')
+for(x=0;x<5;x++)
+y=c[0];
+{
+if (c[x]>y)
+{
+y=c[x];
+}
+}
+printf("The max is%d\n,y");
+}
